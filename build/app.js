426e0bccd2db63305c9ded9ff374dae0926cdf7f
jcalahi <jcalah01@gmail.com>
2019-11-16 20:55:58 +0800

    updated transpiled ts to js

--- build/app.js
+++ build/app.js
@@ -5,29 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var readline_1 = __importDefault(require("readline"));
 var bignumber_js_1 = __importDefault(require("bignumber.js"));
-bignumber_js_1.default.set({ ROUNDING_MODE: bignumber_js_1.default.ROUND_DOWN });
+exports.createList = function (currencyRate) {
+    var currencyList = [];
+    bignumber_js_1.default.set({ ROUNDING_MODE: bignumber_js_1.default.ROUND_DOWN });
+    return function (line) {
+        if (Object.keys(currencyRate).length === 0) {
+            currencyList = line.split(' ');
+            currencyRate = {
+                BTC: new bignumber_js_1.default(currencyList[0]),
+                ETH: new bignumber_js_1.default(currencyList[1]),
+                DOGE: new bignumber_js_1.default(currencyList[2])
+            };
+            return currencyList;
+        }
+        var lineValues = line.split(' ');
+        var saleRate = new bignumber_js_1.default(lineValues[0]);
+        var purchaseAmount = new bignumber_js_1.default(lineValues[3]);
+        var decimalPlaces = parseInt(lineValues[1]);
+        var purchaseCurrency = lineValues[2];
+        var saleAmount = saleRate
+            .multipliedBy(purchaseAmount)
+            .multipliedBy(currencyRate[purchaseCurrency])
+            .dividedBy(currencyRate.ETH)
+            .toFixed(decimalPlaces);
+        console.log(saleAmount); // for logging purposes
+        return saleAmount;
+    };
+};
+var currencyTypes = {};
+var fetchAmount = exports.createList(currencyTypes);
 var rl = readline_1.default.createInterface({
     input: process.stdin,
 });
-var counter = 0;
-var currencies = [];
-var currencyRate;
-rl.on('line', function (line) {
-    counter++;
-    if (counter === 1) {
-        currencies = line.split(' ');
-        currencyRate = {
-            BTC: new bignumber_js_1.default(currencies[0]),
-            ETH: new bignumber_js_1.default(currencies[1]),
-            DOGE: new bignumber_js_1.default(currencies[2])
-        };
-    }
-    else {
-        var lineItems = line.split(' ');
-        var x = new bignumber_js_1.default(lineItems[0]);
-        var y = new bignumber_js_1.default(lineItems[3]);
-        var decimalPlaces = parseInt(lineItems[1]);
-        var initial = x.multipliedBy(y).multipliedBy(currencyRate[lineItems[2]]).dividedBy(currencyRate.ETH);
-        console.log(initial.toFixed(decimalPlaces));
-    }
-});
+rl.on('line', fetchAmount);
